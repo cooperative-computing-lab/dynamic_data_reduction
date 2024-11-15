cf21de56d0c0b129d30ae13fd1e53c0aa45a7025
Benjamin Tovar <btovar@nd.edu>
2024-11-15 12:56:48 -0500

    small example working

--- dynmapred.py
+++ dynmapred.py
@@ -2,24 +2,25 @@
 
 import dask
 import dask.array as da
+
 from functools import reduce
 import cloudpickle
-import lz4
+import lz4.frame
 import sys
 
 import ndcctools.taskvine as vine
 
 
-def wrap_processing(processor, datum, local_executor="threads", local_executor_args=None):
+def wrap_processing(processor, source_connector, datum, local_executor="threads", local_executor_args=None):
     if not local_executor_args:
         local_executor_args = {}
 
-    def proc_task():
-        result = processor(datum).compute(executor=local_executor, **local_executor_args)
-        with lz4.frame.open("out.p", "wb") as fp:
-            cloudpickle.dump(result, fp)
+    datum_post = source_connector(datum)
 
-    return proc_task
+    result = processor(datum_post).compute(executor=local_executor, **local_executor_args)
+    with lz4.frame.open("proc_out.p", "wb") as fp:
+        cloudpickle.dump(result, fp)
+    return result
 
 
 def accumulate(accumulator, result_names, to_file=True):
@@ -36,7 +37,37 @@ def accumulate(accumulator, result_names, to_file=True):
             del other
 
     if to_file:
-        with lz4.frame.open("out.p", "wb") as fp:
+        with lz4.frame.open("accum_out.p", "wb") as fp:
+            cloudpickle.dump(out, fp)
+    else:
+        return out
+
+
+def accumulate_tree(accumulator, result_names, to_file=True, local_executor='threads', local_executor_args=None):
+    import dask
+    if not local_executor_args:
+        local_executor_args = {}
+
+    def load_file(filename):
+        with lz4.frame.open(r, "rb") as fp:
+            return cloudpickle.load(fp)
+
+    to_reduce = []
+    task_graph = {}
+    for r in result_names:
+        key = ("load", len(task_graph))
+        task_graph[key] = (load_file, r)
+        to_reduce.append(key)
+
+    while len(to_reduce) > 1:
+        key = ("merge", len(task_graph))
+        firsts, to_reduce = to_reduce[0:2], to_reduce[2:]
+        task_graph[key] = (accumulator, *firsts)
+        to_reduce.append(key)
+
+    out = dask.get(task_graph, to_reduce[0], executor=local_executor, **local_executor_args)
+    if to_file:
+        with lz4.frame.open("accum_out.p", "wb") as fp:
             cloudpickle.dump(out, fp)
     else:
         return out
@@ -49,8 +80,8 @@ class DynMapReduce:
         self._processor = processor
         self._accumulator = accumulator
 
-        self._max_active = 5
-        self._tasks_active = 0
+        self._max_active = 10
+        self._task_active = set()
 
         self._id_to_output = {}
 
@@ -60,18 +91,45 @@ class DynMapReduce:
 
         self._last_accum_id = None
 
+        self._set_env()
+
+    def _set_env(self, env="env.tar.gz"):
+        functions = [wrap_processing, accumulate, accumulate_tree]
+        # if self.lib_extra_functions:
+        #     functions.extend(self.lib_extra_functions)
+        self._lib_name = f"dynmapred-{id(self)}"
+        libtask = self.manager.create_library_from_functions(
+            self._lib_name,
+            *functions,
+            poncho_env="dummy-value",
+            add_env=False,
+            init_command=None,
+            hoisting_modules=None
+        )
+        envf = self.manager.declare_poncho(env)
+        libtask.add_environment(envf)
+        self.manager.install_library(libtask)
+
+        self._env = envf
+        self._dynmapred_file = self.manager.declare_file(__file__, cache=True)
+        self._coffea_dir = self.manager.declare_file("coffea", cache=True)
+
     @property
     def manager(self):
         return self._manager
 
     def _add_proc_task(self, datum, local_executor='threads', local_executor_args=None):
         result_file = self.manager.declare_temp()
-        fn = wrap_processing(self._processor, datum)
-        task = vine.PythonTask(fn)
+        #task = vine.FunctionCall(self._lib_name, 'wrap_processing', self._processor, datum)
+
+        # task = vine.FunctionCall(self._lib_name, 'wrap_processing', self._processor, datum)
+        task = vine.PythonTask(wrap_processing, self._processor, self._source_connector, datum)
+        # task.add_environment(self._env)
+
         task.set_cores(1)
-        task.add_output(result_file, "out.p")
+        task.add_output(result_file, "proc_out.p")
         task.set_category("processing")
-        self._id_to_output[self.manager.submit(task)] = result_file
+        self._id_to_output[self.submit(task)] = result_file
 
     def _add_accum_tasks(self, temp_result=True):
         if len(self._tasks_to_accumulate) < 1:
@@ -93,7 +151,14 @@ class DynMapReduce:
                 self._tasks_to_accumulate[self._accumulation_size:],
             )
 
-            task = vine.PythonTask(accumulate, self._accumulator, [f"input_{tid}" for tid in firsts])
+            # task = vine.FunctionCall(self._lib_name, "accumulate", self._accumulator, [f"input_{tid}" for tid in firsts])
+            task = vine.PythonTask(
+                accumulate_tree,
+                self._accumulator,
+                [f"input_{tid}" for tid in firsts],
+                local_executor_args={"cores": 4},
+            )
+            # task.add_environment(self._env)
 
             if temp_result:
                 result_file = self.manager.declare_temp()
@@ -101,18 +166,32 @@ class DynMapReduce:
                 result_file = self.manager.declare_file(f"out_{id(self)}")
 
             task.set_cores(1)
-            task.add_output(result_file, "out.p")
+            task.add_output(result_file, "accum_out.p")
 
             for tid in firsts:
                 task.add_input(self._id_to_output[tid], f"input_{tid}")
 
             task.set_category("accumulating")
 
-            tid = self.manager.submit(task)
+            tid = self.submit(task)
             self._id_to_output[tid] = result_file
 
             if set_last:
                 self._last_accum_id = tid
+
+    def wait(self, timeout):
+        t = self.manager.wait(5)
+        if t:
+            self._task_active.remove(t.id)
+            print(f"task {t.id} {t.category} done: {t.result}")
+        return t
+
+    def submit(self, task):
+        task.add_input(self._dynmapred_file, "dynmapred.py")
+        task.add_input(self._coffea_dir, "coffea")
+        tid = self.manager.submit(task)
+        self._task_active.add(tid)
+        return tid
 
     def compute(
         self,
@@ -125,18 +204,21 @@ class DynMapReduce:
 
         self._add_proc_task(next(data))
 
-        for d in data:
-            self._add_proc_task(d)
-
+        last_id = None
         while not self.manager.empty():
-            t = self.manager.wait(5)
+            for d in data:
+                if len(self._task_active) >= self._max_active:
+                    break
+                self._add_proc_task(d)
+
+            t = self.wait(5)
             if t:
-                print(f"task {t.id} {t.category} done: {t.result}")
                 if t.successful():
                     last_id = t.id
                     self._tasks_to_accumulate.append(t.id)
                     self._add_accum_tasks()
                 else:
+                    print("-", t.output)
                     print(t.std_output)
 
         if last_id:
@@ -146,41 +228,39 @@ class DynMapReduce:
             return final_result
 
 
-data = [da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 10000)]
+if __name__ == '__main__':
+    data = (da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 1000))
+#data = [da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 10000)]
 
+    def double_data(d):
+        return d.map_blocks(lambda x: x * 2)
 
-def double_data(d):
-    return d.map_blocks(lambda x: x * 2)
+    def add_data(a, b):
+        return a + b
 
-
-def add_data(a, b):
-    return a + b
-
-
-mgr = vine.Manager(port=9123)
-
-
-dmr = DynMapReduce(mgr, source_connector=lambda x: x, processor=double_data, accumulator=add_data)
-
-
-workers = vine.Factory("local", manager=mgr)
-workers.max_workers = 4
-workers.min_workers = 4
-workers.cores = 4
-workers.memory = 2000
-workers.disk = 8000
-
-with workers:
+    mgr = vine.Manager(port=9123)
+    dmr = DynMapReduce(mgr, source_connector=lambda x: x, processor=double_data, accumulator=add_data)
     result = dmr.compute(data)
+
+# workers = vine.Factory("local", manager=mgr)
+# workers.max_workers = 1
+# workers.min_workers = 0
+# workers.cores = 4
+# workers.memory = 2000
+# workers.disk = 8000
+#
+# with workers:
+#     result = dmr.compute(data)
+
     print(result)
 
 
 
-sys.exit(0)
+    sys.exit(0)
 
 
 
-c = [double_data(d).compute() for d in data]
-print(c)
+    c = [double_data(d).compute() for d in data]
+    print(c)
 
 
---
+++ mdv3.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python
+
+from dynmapred import DynMapReduce
+from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
+import hist.dask as dhist
+import awkward as ak
+import ndcctools.taskvine as vine
+
+data_root = "/afs/crc.nd.edu/user/b/btovar/src/dynmapred/data/samples"
+data = [
+    {
+        "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_11.root",
+        "object_path": "Events",
+        "metadata": "some dataset",
+    },
+    {
+        "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_12.root",
+        "object_path": "Events",
+        "metadata": "some dataset",
+    },
+    {
+        "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_13.root",
+        "object_path": "Events",
+        "metadata": "some dataset",
+    },
+    {
+        "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_14.root",
+        "object_path": "Events",
+        "metadata": "some dataset",
+    },
+    {
+        "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_15.root",
+        "object_path": "Events",
+        "metadata": "some dataset",
+    },
+]
+
+
+def source_connector(file_info):
+    d = {
+        file_info["file"]: {"object_path": "Events", "metadata": file_info["metadata"]}
+    }
+    events = NanoEventsFactory.from_root(
+        d,
+        schemaclass=PFNanoAODSchema,
+        metadata={"dataset": "some_datset"},
+    )
+
+    return events.events()
+
+
+def processor(events):
+    fatjet = events.FatJet
+    genhiggs = events.GenPart[
+        (events.GenPart.pdgId == 25)
+        & events.GenPart.hasFlags(["fromHardProcess", "isLastCopy"])
+    ]
+    parents = events.FatJet.nearest(genhiggs, threshold=0.1)
+    higgs_jets = ~ak.is_none(parents, axis=1)
+
+    cut = (
+        (fatjet.pt > 300)
+        & (fatjet.msoftdrop > 110)
+        & (fatjet.msoftdrop < 140)
+        & (abs(fatjet.eta) < 2.5)
+    ) & (
+        higgs_jets
+    )  # & (fatjet.btagDDBvLV2 > 0.20)
+
+    boosted_fatjet = fatjet[cut]
+    boosted_fatjet.constituents.pf["pt"] = (
+        boosted_fatjet.constituents.pf.pt
+        * boosted_fatjet.constituents.pf.puppiWeight
+    )
+    btag = dhist.Hist.new.Reg(40, 0, 1, name="Btag", label="Btag").Weight()
+    btag.fill(Btag=ak.flatten(boosted_fatjet.btagDDBvLV2))
+
+    return btag
+
+
+def accumulator(a, b):
+    a += b
+    return b
+
+# import dynmapred
+#
+# print(
+#     accumulator(
+#         dynmapred.wrap_processing(processor, source_connector(data[0])),
+#         dynmapred.wrap_processing(processor, source_connector(data[1])),
+#     )
+# )
+#
+# import sys
+# sys.exit(0)
+
+mgr = vine.Manager(port=9123)
+dmr = DynMapReduce(mgr, source_connector=source_connector, processor=processor, accumulator=accumulator)
+result = dmr.compute(data)
+
+print(result)
