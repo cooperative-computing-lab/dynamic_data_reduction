5ceab2be077b92cfd36f2a874fafd060027fcfad
Benjamin Tovar <btovar@nd.edu>
2024-11-21 13:55:46 -0500

    funcs_kwargs

--- dynmapred.py
+++ dynmapred.py
@@ -10,13 +10,15 @@ from dataclasses import dataclass
 from collections import defaultdict
 import ndcctools.taskvine as vine
 
-from typing import Any, Callable, Mapping, Optional, TypeVar
+from typing import Any, Callable, Dict, Mapping, Optional, TypeVar
 
 
 def wrap_processing(
     processor,
     source_connector,
     datum,
+    processor_args=None,
+    source_connector_args=None,
     local_executor="threads",
     local_executor_args=None,
 ):
@@ -26,18 +28,27 @@ def wrap_processing(
         local_executor_args = {}
     local_executor_args.setdefault("nthreads", os.environ.get("CORES", 1))
 
-    datum_post = source_connector(datum)
+    if processor_args is None:
+        processor_args = {}
 
-    result = processor(datum_post).compute(
+    if source_connector_args is None:
+        source_connector_args = {}
+
+    datum_post = source_connector(datum, **source_connector_args)
+
+    result = processor(datum_post, **processor_args).compute(
         executor=local_executor, **local_executor_args
     )
     with lz4.frame.open("proc_out.p", "wb") as fp:
         cloudpickle.dump(result, fp)
-    return result
+    print(result)
 
 
-def accumulate(accumulator, result_names, to_file=True):
+def accumulate(accumulator, result_names, accumulator_args=None, to_file=True):
     out = None
+
+    if accumulator_args is None:
+        accumulator_args = {}
 
     for r in sorted(result_names):
         with lz4.frame.open(r, "rb") as fp:
@@ -46,6 +57,7 @@ def accumulate(accumulator, result_names, to_file=True):
         if out is None:
             out = other
         else:
+            #out = accumulator(out, other, **accumulator_args)
             out = accumulator(out, other)
             del other
 
@@ -59,17 +71,23 @@ def accumulate(accumulator, result_names, to_file=True):
 def accumulate_tree(
     accumulator,
     results,
+    accumulator_n_args=2,
     to_file=True,
     from_files=True,
     local_executor="threads",
     local_executor_args=None,
+    accumulator_args=None,
 ):
     import dask
     import os
+    from functools import partial
 
     if not local_executor_args:
         local_executor_args = {}
     local_executor_args.setdefault("nthreads", os.environ.get("CORES", 1))
+
+    if accumulator_args is None:
+        accumulator_args = {}
 
     if from_files:
 
@@ -82,6 +100,8 @@ def accumulate_tree(
         def load(result):
             return result
 
+    accumulator_w_kwargs = partial(accumulator, **accumulator_args)
+
     to_reduce = []
     task_graph = {}
     for r in results:
@@ -91,8 +111,8 @@ def accumulate_tree(
 
     while len(to_reduce) > 1:
         key = ("merge", len(task_graph))
-        firsts, to_reduce = to_reduce[:2], to_reduce[2:]
-        task_graph[key] = (accumulator, *firsts)
+        firsts, to_reduce = to_reduce[:accumulator_n_args], to_reduce[accumulator_n_args:]
+        task_graph[key] = (accumulator_w_kwargs, *firsts)
         to_reduce.append(key)
 
     out = dask.get(
@@ -101,14 +121,15 @@ def accumulate_tree(
     if to_file:
         with lz4.frame.open("accum_out.p", "wb") as fp:
             cloudpickle.dump(out, fp)
-    return out
+    else:
+        return out
 
 
-def identity_source_conector(datum):
+def identity_source_conector(datum, **extra_args):
     return datum
 
 
-def default_accumualtor(a, b):
+def default_accumualtor(a, b, **extra_args):
     return a + b
 
 
@@ -124,10 +145,13 @@ class DynMapReduce:
     accumulator: Callable[[H, H], H] = default_accumualtor
     max_tasks_active: Optional[int] = None
     max_tasks_per_dataset: Optional[int] = None
-    max_task_retries: int = 10
+    max_task_retries: int = 2
     accumulation_size: int = 10
     resources_processing: Optional[Mapping[str, float]] = None
     resources_accumualting: Optional[Mapping[str, float]] = None
+    processor_args: Optional[Mapping[str, Any]] = None
+    source_connector_args: Optional[Mapping[str, Any]] = None
+    accumulator_args: Optional[Mapping[str, Any]] = None
     environment: Optional[str] = None
     x509_proxy: Optional[str] = None
 
@@ -175,7 +199,7 @@ class DynMapReduce:
             self._proxy_file = self.manager.declare_file(self.x509_proxy, cache=True)
 
     def _add_fetch_task(self, result_task):
-        task = vine.Task("cp in out")
+        task = vine.Task("cp -r in out")
 
         ds = result_task.metadata["dataset"]
 
@@ -202,20 +226,20 @@ class DynMapReduce:
             )
 
     def _add_proc_task(
-        self, datum, dataset, attempt=1, local_executor="threads", local_executor_args=None
+        self, datum, dataset, local_executor="threads", local_executor_args=None
     ):
         result_file = self.manager.declare_temp()
 
         # task = vine.FunctionCall(self._lib_name, 'wrap_processing', self._processor, datum)
         task = vine.PythonTask(
-            wrap_processing, self.processor, self.source_connector, datum
+            wrap_processing, self.processor, self.source_connector, datum, self.processor_args, self.source_connector_args,
         )
         if self.environment:
             task.add_environment(self.environment)
 
         ds = dataset
         task.set_category(f"processing#{ds}")
-        task.metadata = {"type": "processing", "dataset": ds, "input": datum, "retries": attempt}
+        task.metadata = {"type": "processing", "dataset": ds, "input": datum}
         task.add_output(result_file, "proc_out.p")
         self._id_to_output[self.submit(task)] = result_file
 
@@ -243,28 +267,33 @@ class DynMapReduce:
 
         # task = vine.FunctionCall(self._lib_name, "accumulate", self._accumulator, [f"input_{tid}" for tid in firsts])
         task = vine.PythonTask(
-            accumulate_tree,
-            #accumulate,
+            #accumulate_tree,
+            accumulate,
             self.accumulator,
             [f"input_{t.id}" for t in firsts],
+            accumulator_args=self.accumulator_args,
+            to_file=True,
         )
 
-        result_file = self.manager.declare_temp()
+        #result_file = self.manager.declare_temp()
+        result_file = self.manager.declare_file("o", cache=True)
         task.add_output(result_file, "accum_out.p")
 
         for t in firsts:
             task.add_input(self._id_to_output[t.id], f"input_{t.id}")
 
-        task.set_cores(1)
         task.set_category(f"accumulating#{ds}")
-        task.metadata = {"type": "processing", "dataset": ds}
-        task.add_output(result_file, "proc_out.p")
+        task.metadata = {"type": "accumulating", "dataset": ds}
+
         self._id_to_output[self.submit(task)] = result_file
 
     def wait(self, timeout):
         t = self.manager.wait(5)
         if t:
-            print(f"task {t.id} '{t.category}' done: {t.result} status: {t.exit_code}")
+            print(f"task {t.id} '{t.category}' exit code: {t.exit_code}, vine status: {t.result}")
+            print(f"{t.output}")
+            print(f"{t.std_output}")
+
             ds = t.metadata["dataset"]
             self._task_active -= 1
             self._ds_active_count[ds] -= 1
@@ -272,13 +301,11 @@ class DynMapReduce:
                 self._ds_done_count[ds] += 1
                 self._ds_to_accumulate[ds].append(t)
             else:
-                if (
-                    t.metadata["type"] == "processing"
-                    and t.metadata["retries"] < self.max_task_retries
-                ):
-                    print(f"resubmitting task {t.id} attempts so far: {t.metadata['retries']}")
+                should_retry = t.metadata["type"] == "processing" and t.metadata["attempt"] < self.max_task_retries
+                if should_retry:
+                    print(f"resubmitting task {t.id} attempts so far: {t.metadata['attempt']}")
                     self._add_proc_task(
-                        t.metadata["input"], ds, attempt=t.metadata["retries"] + 1
+                        t.metadata["input"], ds,
                     )
                 else:
                     raise RuntimeError(
@@ -286,10 +313,16 @@ class DynMapReduce:
                     )
         return t
 
-    def submit(self, task):
+    def submit(self, task, attempt=None):
         task.add_input(self._dynmapred_file, "dynmapred.py")
         task.add_input(self._coffea_dir, "coffea")
         task.set_retries(self.max_task_retries)
+
+        new_attempt = attempt
+        if not new_attempt:
+            new_attempt = task.metadata.get("attempt", 0) + 1
+
+        task.metadata["attempt"] = new_attempt
 
         if self._proxy_file:
             task.add_input(self._proxy_file, "proxy.pem")
@@ -347,29 +380,27 @@ class DynMapReduce:
 
 
 if __name__ == "__main__":
-    data = (da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 1000))
+    data = {"some_ds": (da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 3))}
     # data = [da.from_array(list(range(0, n * 1000000, n))[0:10]) for n in range(1, 10000)]
 
-    def double_data(d):
-        return d.map_blocks(lambda x: x * 2)
+    def double_data(datum):
+        return datum.map_blocks(lambda x: x * 2)
 
     def add_data(a, b):
         return a + b
 
-    mgr = vine.Manager(port=9123)
+    mgr = vine.Manager(port=[9123, 9129], name="btovar-dynmapred")
     dmr = DynMapReduce(
         mgr, source_connector=lambda x: x, processor=double_data, accumulator=add_data
     )
-    result = dmr.compute(data)
 
-    # workers = vine.Factory("local", manager=mgr)
-    # workers.max_workers = 1
-    # workers.min_workers = 0
-    # workers.cores = 4
-    # workers.memory = 2000
-    # workers.disk = 8000
-    #
-    # with workers:
-    #     result = dmr.compute(data)
+    workers = vine.Factory("local", manager=mgr)
+    workers.max_workers = 1
+    workers.min_workers = 0
+    workers.cores = 4
+    workers.memory = 2000
+    workers.disk = 8000
+    with workers:
+        result = dmr.compute(data)
 
     print(result)
--- mdv3.py
+++ mdv3.py
@@ -1,78 +1,16 @@
 #! /usr/bin/env python
 
 from dynmapred import DynMapReduce
-from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
-import hist.dask as dhist
-import awkward as ak
 import ndcctools.taskvine as vine
 import cloudpickle
-
-data_root = "/afs/crc.nd.edu/user/b/btovar/src/dynmapred/data/samples"
-data = {
-    "some dataset A": [
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_11.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset A"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_12.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset A"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_13.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset A"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_14.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset A"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_15.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset A"},
-        },
-    ],
-
-    "some dataset B": [
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_11.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset B"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_12.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset B"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_13.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset B"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_14.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset B"},
-        },
-        {
-            "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_15.root",
-            "object_path": "Events",
-            "metadata": {"dataset": "some dataset B"},
-        },
-    ]
-}
-
-with open("dv3_preprocessed.pkl", "rb") as f:
-    data = cloudpickle.load(f)
-
-    # data = {"hbb": data["hbb"]}
+import pprint
 
 
-def source_connector(file_info):
+def source_connector(file_info, **source_args):
+    from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
+    import math
+    import os
+
     source_root = "root://hactar01.crc.nd.edu//store/user/cmoore24/samples/"
     source_ceph = "/cms/cephfs/data/store/user/cmoore24/samples"
     file_info["file"] = file_info["file"].replace(source_ceph, source_root)
@@ -82,6 +20,18 @@ def source_connector(file_info):
             "object_path": "Events",
         }
     }
+
+    num_entries = file_info.get("num_entries", None)
+    if num_entries and num_entries > 0:
+        cores = source_args.get("cores", os.environ.get("CORES", 1))
+        step = source_args.get("step", math.ceil(file_info["num_entries"] / cores))
+        steps = []
+        start = 0
+        while start < num_entries:
+            end = start + min(start + step, num_entries)
+            steps.append([start, end])
+            start = end
+        d[file_info["file"]["steps"]] = steps
 
     events = NanoEventsFactory.from_root(
         d,
@@ -94,6 +44,9 @@ def source_connector(file_info):
 
 
 def processor(events):
+    import hist.dask as dhist
+    import awkward as ak
+
     label = events.metadata["label"]
     fatjet = events.FatJet
 
@@ -139,35 +92,89 @@ def accumulator(a, b):
     a += b
     return a
 
-# import dynmapred
-#
-# print(
-#     accumulator(
-#         dynmapred.wrap_processing(processor, source_connector(data[0])),
-#         dynmapred.wrap_processing(processor, source_connector(data[1])),
-#     )
-# )
-#
-# import sys
-# sys.exit(0)
+
+if __name__ == '__main__':
+    data_root = "/afs/crc.nd.edu/user/b/btovar/src/dynmapred/data/samples"
+    data = {
+        "some dataset A": [
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_11.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset A"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_12.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset A"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_13.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset A"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_14.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset A"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_15.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset A"},
+            },
+        ],
+
+        "some dataset B": [
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_11.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset B"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_12.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset B"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_13.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset B"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_14.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset B"},
+            },
+            {
+                "file": f"{data_root}/flat400/mass100/RunIISummer20UL17PFNANOAODSIM_15.root",
+                "object_path": "Events",
+                "metadata": {"dataset": "some dataset B"},
+            },
+        ]
+    }
+
+    with open("dv3_preprocessed.pkl", "rb") as f:
+        data = cloudpickle.load(f)
+
+        # data = {"hbb": data["hbb"]}
 
 
-mgr = vine.Manager(port=0, name="btovar-dynmapred")
-dmr = DynMapReduce(
-    mgr,
-    source_connector=source_connector,
-    processor=processor,
-    accumulator=accumulator,
-    accumulation_size=20,
-    max_tasks_active=1000,
-    #max_tasks_per_dataset=8,
-    x509_proxy="x509up_u196886",
-)
 
-result = dmr.compute(data)
 
-with open("result_ac.pkl", "wb") as f:
-    cloudpickle.dump(result, f)
+    mgr = vine.Manager(port=0, name="btovar-dynmapred")
+    dmr = DynMapReduce(
+        mgr,
+        source_connector=source_connector,
+        processor=processor,
+        accumulator=accumulator,
+        accumulation_size=20,
+        max_tasks_active=600,
+        max_tasks_per_dataset=8,
+        x509_proxy="x509up_u196886",
+    )
 
-import pprint
-pprint.pprint(result)
+    result = dmr.compute(data)
+
+    with open("result_ac.pkl", "wb") as fw:
+        cloudpickle.dump(result, fw)
+    pprint.pprint(result)
