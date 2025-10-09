22095f70e5820cde5fe58d5363d65b1e34d4f1c2
Benjamin Tovar <btovar@nd.edu>
2025-10-09 11:25:22 -0400

    rename to dynamic_data_reduction

--- src/dynamic_data_reduction/_version.py
+++ src/dynamic_data_reduction/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev87+g70848124b.d20251002'
-__version_tuple__ = version_tuple = (0, 1, 'dev87', 'g70848124b.d20251002')
+__version__ = version = '0.1.dev88+ga387a842d.d20251009'
+__version_tuple__ = version_tuple = (0, 1, 'dev88', 'ga387a842d.d20251009')
 
-__commit_id__ = commit_id = 'g70848124b'
+__commit_id__ = commit_id = 'ga387a842d'
